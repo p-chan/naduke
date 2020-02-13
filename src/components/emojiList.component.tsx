abe3a9fc8234ccdc1c64cda61dc32e91534ae682
P-Chan <hello@0x50.io>
2020-02-14 06:05:09 +0900

    feat: Update EmojiList component

--- src/components/emojiList.component.tsx
+++ src/components/emojiList.component.tsx
@@ -4,12 +4,51 @@ import { emojiModel } from '../models'
 
 import { UserContainer } from '../containers'
 
+type IntegratedEmojiModel = {
+  name: string
+  url: string
+  aliases: string[]
+}
+
 export const EmojiListComponent: React.FC = () => {
   const user = UserContainer.useContainer()
 
   const [emojiListState, setEmojiListState] = React.useState<
     emojiModel.EmojiModel[]
   >([])
+
+  const [
+    integratedEmojiListState,
+    setIntegratedEmojiListState
+  ] = React.useState<IntegratedEmojiModel[]>([])
+
+  React.useEffect(() => {
+    const integratedEmojiObject: { [key: string]: IntegratedEmojiModel } = {}
+
+    for (let i = 0; i < emojiListState.length; i++) {
+      const item = emojiListState[i]
+
+      if (item.type === 'original') {
+        integratedEmojiObject[item.name] = {
+          name: item.name,
+          url: item.url,
+          aliases: []
+        }
+      }
+    }
+
+    for (let i = 0; i < emojiListState.length; i++) {
+      const item = emojiListState[i]
+
+      if (item.type === 'alias') {
+        if (item.name !== 'white_square' && item.name !== 'black_square') {
+          integratedEmojiObject[item.originalName]['aliases'].push(item.name)
+        }
+      }
+    }
+
+    setIntegratedEmojiListState(Object.values(integratedEmojiObject))
+  }, [emojiListState])
 
   React.useEffect(() => {
     const getEmojiListData = async () => {
@@ -28,13 +67,12 @@ export const EmojiListComponent: React.FC = () => {
     })
   }, [])
 
-  const emojiListItemElements = emojiListState.map(item => {
-    if (item.type === 'alias') return
-
+  const emojiListItemElements = integratedEmojiListState.map(item => {
     return (
       <li key={item.name}>
         <img src={item.url} width={128} height={128} alt={item.name} />
         <span>{item.name}</span>
+        <span>{item.aliases}</span>
       </li>
     )
   })
